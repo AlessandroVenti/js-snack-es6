e4fff1d5b9a0e2bf02cbeb22a091d9b74190b0bc
Alessandro Venti <alessandro.venti@icloud.com>
2021-03-26 14:49:30 +0100

    exercise 2 1st part done

--- js/script.js
+++ js/script.js
@@ -62,19 +62,43 @@ function findLightBike2() {
 // $(document).ready(findLightBike2);
 
 
-
-                                        // Creare un array di oggetti di squadre di calcio. 
-                                   // Ogni squadra avrà diverse proprietà: 
-                                   // nome, punti fatti, falli subiti.
-                                   // Nome sarà l'unica proprietà da compilare, 
-                                   // le altre saranno tutte settate a 0.
-                                   // Generare numeri random al posto degli 0 nelle proprietà:
-                                   // punti fatti e falli subiti
-                                    // Usando la destrutturazione creiamo un nuovo array 
-                                   // i cui elementi contengono solo nomi e falli subiti 
-                                   // e stampiamo tutto in console.
+                                   //Esercizio 2
 
 
 
+                                        //1 
+                                        //Creare un array di oggetti di squadre di calcio. 
+                                        // Ogni squadra avrà diverse proprietà: 
+                                        // nome, punti fatti, falli subiti.
+                                        // Nome sarà l'unica proprietà da compilare, 
+                                        // le altre saranno tutte settate a 0.
+
+
+function footballDatabase() {
+
+     var squads = [
+          { 'name' : 'Roma', 'punti' : 0, 'falli' : 0 },
+          { 'name' : 'Milan', 'punti' : 0, 'falli' : 0 },
+          { 'name' : 'Inter', 'punti' : 0, 'falli' : 0 },
+          { 'name' : 'Napoli', 'punti' : 0, 'falli' : 0 },
+          { 'name' : 'Parma', 'punti' : 0, 'falli' : 0 },
+          { 'name' : 'Juventus', 'punti' : 0, 'falli' : 0 },
+          { 'name' : 'Brescia', 'punti' : 0, 'falli' : 0 }
+     ];
+
+     console.log(squads);
+     return squads;
+}
+
+// $(document).ready(footballDatabase);
+
+
+                                             //2 
+                                        // Generare numeri random al posto degli 0 nelle proprietà:
+                                        // punti fatti e falli subiti
+                                        //3
+                                        // Usando la destrutturazione creiamo un nuovo array 
+                                        // i cui elementi contengono solo nomi e falli subiti 
+                                        // e stampiamo tutto in console.
 
    
