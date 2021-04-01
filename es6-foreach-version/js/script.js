56d7ed44a9487e5ca830f8525cbdbe97da2a11cd
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-01 15:06:35 +0200

    Bike exercise done

--- es6-foreach-version/js/script.js
+++ es6-foreach-version/js/script.js
@@ -13,13 +13,76 @@ function generateArray (numberOfElements,minRangeOfNumbers,maxRangeOfNumbers) {
      console.log(`Current Array, ${newArray}`);
      return newArray;
 }
+
+
+
 function rangeYourArray(minRange, maxRange, array) {
      let rangedArray = array.filter((element, index) => index >= minRange && index <= maxRange)
      return console.log(`New Ranged Array : ${rangedArray}`);
 }
-$(document).ready(rangeYourArray( 
-                                   parseInt(prompt(`Inserisci un range minimo...`)),
-                                   parseInt(prompt(`Inserisci un range massimo...`)),
-                                   generateArray(10, 1, 10)));
+// $(document).ready(rangeYourArray( 
+//                                    parseInt(prompt(`Inserisci un range minimo...`)),
+//                                    parseInt(prompt(`Inserisci un range massimo...`)),
+//                                    generateArray(10, 1, 10))
+//                                 );
+
+
+                                        // ---> Es1 <---
+                                        // Creare un array di oggetti: ogni oggetto descriverà 
+                                        // una bici da corsa con le seguenti proprietà: nome e peso. 
+                                        // Stampare a schermo la bici con peso minore.
+
+function bikesArray() {                                                 
+     return [
+          { 'name' : 'Gianluca', 'weight' : 30 },
+          { 'name' : 'Antonio', 'weight' : 20 },
+          { 'name' : 'Olga', 'weight' : 10 },
+          { 'name' : 'Judy', 'weight' : 11 },
+          { 'name' : 'Panam', 'weight' : 15 },
+          { 'name' : 'Lorenzo', 'weight' : 25 },
+     ];
+}
+
+
+function findLightBike(array) {
+     const minWeightBikes = [];
+     array.forEach(element => minWeightBikes.push(element.weight));
+     const minWeight = Math.min(...minWeightBikes);
+     const minWeightBike = array.filter(element => element.weight === minWeight);
+     console.log(`most light bike :`, minWeightBike);
+     return minWeightBike;
+     
+}
+     
+// $(document).ready(findLightBike(bikesArray()));
+
+
+//                                    //Soluzione usando il destructuring
+
+function bikesArray2() {                                                 
+     return [
+          { 'name' : 'Gianluca', 'weight' : 30 },
+          { 'name' : 'Antonio', 'weight' : 20 },
+          { 'name' : 'Olga', 'weight' : 10 },
+          { 'name' : 'Judy', 'weight' : 11 },
+          { 'name' : 'Panam', 'weight' : 15 },
+          { 'name' : 'Lorenzo', 'weight' : 25 },
+     ];
+}
+
+function findLightBike2(array) {
+     
+     let lightBike = { name: '', weight: Number.MAX_SAFE_INTEGER };
+     array.forEach(element => {
+          let {name, weight} = element;
+          if( weight < lightBike.weight) {
+               lightBike = element;
+          }
+     });
+     console.log(lightBike);
+     return lightBike;
+}
+
+$(document).ready(findLightBike2(bikesArray2()));
 
 
